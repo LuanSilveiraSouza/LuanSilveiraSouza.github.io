accef7387102a2090530381094da9ef2feaccf50
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-04-01 14:43:48 -0300

    About Section: First Design

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
+const div = document.getElementById('link');
+
 setInterval(changeBackground, 2000);
 
 function changeBackground() {
-  const div = document.getElementById('link');
-
   if(div.classList.contains('bg-blue')) {
-    div.classList.remove('bg-blue');
-    div.classList.add('bg-white');
+    bgWhite();
   } else {
-    div.classList.remove('bg-white');
-    div.classList.add('bg-blue');
+    bgBlue();
   }
 }
+
+const bgWhite = () => {
+  div.classList.remove('bg-blue');
+  div.classList.add('bg-white');
+}
+
+const bgBlue = () => {
+  div.classList.remove('bg-white');
+  div.classList.add('bg-blue');
+} 
